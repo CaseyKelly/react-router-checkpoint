3e8d83b6e63781116a3c0a2b50169599026bd6c5
Casey Kelly <casey.kelly@dish.com>
2018-03-12 12:43:09 -0600

    auto redirect from / to /login

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import React from 'react'
-import Login from './components/Login'
-import TopNav from './components/TopNav'
-import './App.css'
+import React from 'react';
+import Login from './components/Login';
+import TopNav from './components/TopNav';
+import { Route, Redirect } from 'react-router-dom';
+import './App.css';
 
 export const App = () => {
   return (
     <div>
+      <Route exact path="/" render={() => <Redirect to="/login" />} />
       <TopNav />
-      <Login/>
+      <Login />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import store from './store';
-import 'bootswatch/dist/materia/bootstrap.min.css'
+import 'bootswatch/dist/materia/bootstrap.min.css';
 import './index.css';
 
 const storeInstance = store();
 
 ReactDOM.render(
   <Provider store={storeInstance}>
-   <App />
- </Provider>,
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
